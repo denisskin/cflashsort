df7f8d05851ba2cb70aabcac6ad71a5fb39469a0
Denis Glazkov <denis.glazkov@lazada.com>
2015-07-05 12:53:38 +0300

    add benchmarks

---
+++ benchmarks/benchmark.c
@@ -0,0 +1,118 @@
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "../src/flashsort.h"
+
+void benchmark(const char* filename);
+
+int main() {
+    benchmark("data/words-en.txt");
+    benchmark("data/hashes-hex.txt");
+    benchmark("data/hashes-base64.txt");
+    benchmark("data/numbers.txt");
+    benchmark("data/numbers2.txt");
+    return 0;
+}
+
+static int cmpfunc(const void * a, const void * b) {
+    return strcmp(*(char**)a, *(char**)b);
+}
+
+void benchmark(const char* filename) {
+
+    //------- read file ---------------
+    int i, nValues = 0;
+    FILE *fp = fopen(filename, "r");
+    if(!fp) {
+        printf("Error: cant not open file %s!\n", filename);
+        exit(1);
+    }
+    fseek(fp, 0, SEEK_END);
+    long fileLen = ftell(fp);
+    char *buf = malloc(fileLen+1), *buf0=buf;
+    if(!buf) {
+        printf("ERROR: Can`t allocate memory! (%ld bytes)\n", fileLen);
+        exit(-1);
+    }
+    fseek(fp, 0, 0);
+    while(!feof(fp)) nValues += !!fgets(buf, 100, fp);
+
+    char **values  = (char**)malloc(nValues * sizeof(char*));
+    char **values1 = (char**)malloc(nValues * sizeof(char*));
+    char **values2 = (char**)malloc(nValues * sizeof(char*));
+    if(!values) {
+        printf("ERROR: Can`t allocate memory! (%ld bytes)\n", i * sizeof(char*));
+        exit(1);
+    }
+    fseek(fp, 0, 0);
+    for(i = 0; !feof(fp); i++) {
+        values[i] = fgets(buf, 100, fp);
+        int len = strlen(buf);
+        buf[len-1] = 0;
+        buf += len;
+    }
+
+    printf("\n\n------------ %s -------------", filename);
+    printf("\n total count values: %d", nValues);
+    printf("\n top-10 values: ");
+    for(i=0; i<10; i++) printf(" %s,", values[i]);
+    printf("... \n");
+    printf("\n-------------------------------------------------------------------------------------------------------");
+    printf("\nCount\t\tFlash-sort\t\t          \tQuick-sort");
+    printf("\nelements\ttotal time\tone operation\t\ttotal time\tone operation");
+    printf("\n-------------------------------------------------------------------------------------------------------");
+
+    const int STEPS = 20;   // count of points
+    const int K     = 30;   // count of experiments on one point
+
+    for(int step=1; step<=STEPS; step++) {
+        int n = (int)pow(nValues, 1. * step / STEPS);
+
+        clock_t t0, t;
+        double st1=0, st2=0;
+
+        printf("\n%7d\t", n);
+        for(int k=0; k < K; k++) { // count experiments
+            // init data
+            for(i = 0; i < n; i++) values2[i] = values1[i] = values[i];
+
+            // -- flash sort
+            t0 = clock();
+            flashsort_str(values1, n);
+            t = clock();
+            st1 += (double)(t - t0) / CLOCKS_PER_SEC;
+
+            // -- quick sort
+            t0 = clock();
+            qsort(values2, n, sizeof(char*), cmpfunc);
+            t = clock();
+            st2 += (double)(t - t0) / CLOCKS_PER_SEC;
+        }
+        // print avg time
+        printf("\t%lf sec\t[%lf mcsec]", st1/K, st1/K/n*10e6);
+        printf("\t%lf sec\t[%lf mcsec]", st2/K, st2/K/n*10e6);
+        printf("\t%+-6.2lf%%", (st2/st1-1)*100);    // difference in %
+
+        for(i = 0; i < n; i++) {
+            if(strcmp(values1[i], values2[i])) {
+                printf("\nNOT EQUAL %d (%s != %s)\n", i, values1[i], values2[i]);
+                for(int j=i-5; j<i+5; j++) printf(" %s \t%s \n", values1[j], values2[j]);
+                exit(1);
+            }
+        }
+    }
+    printf("\n\n top-10 sorted data: ");
+    for(i=0; i<10; i++) printf(" %s,", values1[i]);
+    printf("... \n");
+
+    printf("\n\n");
+
+    free(buf0);
+    free(values);
+    free(values1);
+    free(values2);
+}
+
---
+++ src/flashsort_str.c
@@ -0,0 +1,196 @@
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2015 Denis Glazkov
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+#include <stdio.h>
+#include <string.h>
+
+#define STACK_SIZE 1000
+
+#define BYTE(value, lv) (*((char*)(value)+lv)? ((char*)(value)+lv) : 0)
+
+#define SWAP(a, b)  v=b,b=a,a=v
+
+void flashsort_str(char **values, size_t n) {
+
+    typedef char* type;
+    typedef char byte;
+
+    typedef struct {
+        type* pVal;
+        unsigned len;
+    } Bucket;
+
+    typedef struct {
+        int lv;
+        type* pn;
+    } Stack;
+    if(n < 2) return;
+
+    Bucket buckets[256];
+    Bucket *b, *bp, *bLo, *bHi, *bMax = buckets + 256;
+    memset(buckets, 0, 256 * sizeof(Bucket));
+
+    Stack stack[STACK_SIZE] = {{0, values+n}}, *pStack = stack;
+    type v, *p0 = values, *pn, *p;
+    byte *c, *c0, ch, ch0;
+    unsigned lv, countBuckets;
+
+start:
+    lv = pStack->lv;    // level
+    pn = pStack->pn;    // pointer on end of array
+
+    if(pn == p0 + 1) {  // only one element. rise up a level
+        p0 = pn;
+        pStack--;
+        goto sub;
+    }
+    if(pn == p0 + 2) {  // compare 2 element and rise up a level
+        p = p0+1;
+        // short sort
+        for( ; ; lv++) {
+            if(!(c0 = BYTE(*p0, lv))) break;
+            if(!(c = BYTE(*p, lv))) { SWAP(*p, *p0); break; }
+            if(*c != *c0) {
+                if(*c < *c0) SWAP(*p, *p0);
+                break;
+            }
+        }
+        p0 = pn;
+        pStack--;
+        goto sub;
+    }
+
+    bHi = 0;
+    bLo = bMax;
+    countBuckets = 0;
+
+    // move pointer to no null value
+    while(p0 < pn && !BYTE(*p0, lv)) p0++;
+
+    // calc buckets sizes.
+    for(p = p0; p < pn; p++) {
+        c = BYTE(*p, lv);
+        if(c) {
+            countBuckets += !(b = buckets + *c)->len++;
+            if(b < bLo) bLo = b;
+            if(b > bHi) bHi = b;
+        } else {
+            // move NULL-byte value to begin.  p0++
+            SWAP(*p, *p0);
+            p0++;
+        }
+    }
+
+    if(countBuckets == 0) {
+        pStack--;
+        goto sub;
+    }
+
+    if(countBuckets == 1) {
+        bLo->len = 0;
+        pStack->lv++;
+        goto start;
+    }
+
+    if(countBuckets == 2) {
+        pStack++;
+        pStack->lv = lv+1;
+        bLo->pVal = p0;
+        bHi->pVal = pStack->pn = p0 + bLo->len;
+        for(b = bLo; b->len; b->len--, b->pVal++) {
+            while((bp = buckets + *BYTE(*b->pVal, lv)) != b) {
+                SWAP(*b->pVal, *bp->pVal);
+                bp->len--;
+                bp->pVal++;
+            }
+        }
+        bHi->len = 0;
+        goto start;
+    }
+
+    // set scopes of buckets (pointers to value)
+    // move p0 for
+    for(b=bLo; b->len < 2 && b <= bHi; b++) {
+        if(b->len) b->pVal = p0++;
+    }
+    if(p0 != pn) {
+        // pre-set size of first sub.bucket for next iteration
+        pStack++;
+        pStack->pn = p0 + b->len;
+        pStack->lv = lv+1;
+    }
+    for(p=p0; b<=bHi; b++) {
+        b->pVal = p;
+        p += b->len;
+    }
+
+    // move values into appropriate buckets
+    for(b = bLo; b < bHi; b++) {
+        for(; b->len; b->len--, b->pVal++) {
+            while((bp = buckets + *BYTE(*b->pVal, lv)) != b) {
+                SWAP(*b->pVal, *bp->pVal);
+                bp->len--;
+                bp->pVal++;
+            }
+        }
+    }
+    bHi->len = 0;
+
+    if(p0 == pn) { // all buckets has length == 1. rise to up
+        pStack--;
+        goto sub;
+    }
+
+    // start process sub buckets.  process first sub bucket
+    goto start;
+
+sub:
+    // continue processing sub buckets
+
+    if(pStack < stack) return; // finish sort
+    lv = pStack->lv;
+    pn = pStack->pn;
+
+    if(p0 >= pn) {
+        pStack--;
+        goto sub;
+    }
+
+    // calculate length of current sub-bucket
+    ch = *BYTE(*p0, lv);
+    for(p = p0 + 1; p < pn; p++) {
+        if(ch != (ch0 = *BYTE(*p, lv))) break;
+        ch = ch0;
+    }
+
+    pStack++;
+    // todo: check stack size
+//    if(pStack > stack + STACK_SIZE) {
+//        printf("\n\n STACK OVERFLOW \n");
+//        return;
+//    }
+    pStack->lv = lv+1;
+    pStack->pn = p;
+    goto start;
+}
+
+
